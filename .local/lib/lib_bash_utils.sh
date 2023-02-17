1ca69dbf808585b7686e3b2be47a2c80d383e9ac
Stefan Römer <sroemer@github.com>
2023-02-17 19:40:41 +0700

    scripts: add lib_bash_utils for logging and getting latest release versions of github repositories
    Signed-off-by: Stefan Römer <stefan.roemer@senvang.org>

---
+++ .local/lib/lib_bash_utils.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+###
+# Logging to stdout via log() and stderr via error_log()
+#
+# Arguments:
+#   error message
+###
+function log() {
+    echo "INFO : $*"
+}
+function error_log() {
+    echo "ERROR: $*" >&2
+}
+
+###
+# Request latest release version of Github repository
+#
+# Arguments:
+#   repository owner, repository name
+###
+function get_github_latest_release_version() {
+
+    # check if curl and jq are available
+    if ! [[ -x $(command -v curl) ]]; then
+        return 1
+    fi
+    if ! [[ -x $(command -v jq) ]]; then
+        return 1
+    fi
+
+    # check arguments (owner and repo)
+    local owner="$1"
+    local repo="$2"
+    if [[ -z "$owner" ]]; then
+        return 1
+    fi
+    if [[ -z "$repo" ]]; then
+        return 1
+    fi
+
+    # perform the request of the latest release version
+    if ! version=$(curl -sL https://api.github.com/repos/"$owner"/"$repo"/releases/latest | jq -reM ".tag_name"); then
+        return 1
+    fi
+
+    # return version on success
+    echo "$version"
+    return 0
+}
